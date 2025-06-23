8db13438c8432d9e78b539a30b19d71d1b29485b
mortada <mortada97442@gmail.com>
2025-06-23 20:14:31 +0300

    deleted unused imports

--- lib/widgets/onboarding_indicator.dart
+++ lib/widgets/onboarding_indicator.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../providers/onboarding_conter_provider.dart';
-
 class OnboardingIndicator extends StatelessWidget {
   const OnboardingIndicator({
     super.key,
--- lib/widgets/onboarding_page.dart
+++ lib/widgets/onboarding_page.dart
@@ -1,4 +1,3 @@
-import 'package:algad_almushriq/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
