bcfc6c88dd3bbd420942003f847fb856bd6f167f
mortada <mortada97442@gmail.com>
2025-06-24 20:32:15 +0300

    almost finished profile screen

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -1,11 +1,20 @@
+import 'package:algad_almushriq/router/route_names.dart';
 import 'package:algad_almushriq/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:algad_almushriq/utlis/widgets/option_card.dart';
 
-import '../utlis/widgets/option_card.dart';
-
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool isEnglish = true;
+  bool isNotification = true;
 
   @override
   Widget build(BuildContext context) {
@@ -19,55 +28,123 @@ class ProfileScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        spacing: Checkbox.width,
-        textDirection: TextDirection.rtl,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 70,
-            width: 340,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(30)),
-                side: BorderSide(),
-              ),
-              elevation: 5,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 6),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      child: HugeIcon(
-                        icon: HugeIcons.strokeRoundedUser,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          textDirection: TextDirection.rtl,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 70,
+              width: double.infinity,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  side: BorderSide(color: context.colorScheme.surface),
+                ),
+                elevation: 5,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    textDirection: TextDirection.rtl,
+                    children: [
+                      CircleAvatar(
+                        radius: 20,
+                        child: HugeIcon(
+                          icon: HugeIcons.strokeRoundedUser02,
+                          color: context.colorScheme.surface,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [Text('مرتضى غسان'), Text('07700000000')],
+                      ),
+                      SizedBox(width: 120),
+                      HugeIcon(
+                        icon: HugeIcons.strokeRoundedPencilEdit01,
                         color: context.colorScheme.primary,
                       ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [Text('مرتضى غسان'), Text('07700000000')],
-                    ),
-                    SizedBox(width: 120),
-                    HugeIcon(
-                      icon: HugeIcons.strokeRoundedPencilEdit01,
-                      color: context.colorScheme.primary,
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Text('الاعدادات', style: TextStyle(fontSize: 16)),
-          OptionCard(
-            leadingIcon: HugeIcons.strokeRoundedNotification01,
-            text: 'الاشعارات',
-            value: false,
-          ),
-        ],
+            Text('الاعدادات', style: TextStyle(fontSize: 16)),
+            SizedBox(height: 16),
+            OptionCard(
+              leadingIcon: HugeIcons.strokeRoundedNotification01,
+
+              text: 'الاشعارات',
+              trailing: Switch.adaptive(
+                value: isNotification,
+                activeColor: context.colorScheme.primary,
+                inactiveThumbColor: context.colorScheme.primary,
+                inactiveTrackColor: context.colorScheme.surface,
+                activeTrackColor: context.colorScheme.shadow,
+                onChanged: (val) {
+                  setState(() {
+                    isNotification = val;
+                  });
+                },
+              ),
+            ),
+            SizedBox(height: 8),
+            OptionCard(
+              leadingIcon: HugeIcons.strokeRoundedGlobe02,
+
+              text: 'اللغة',
+              trailing: ToggleButtons(
+                isSelected: [!isEnglish, isEnglish],
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+                onPressed: (index) {
+                  setState(() {
+                    isEnglish = index == 1;
+                  });
+                },
+                selectedColor: Colors.white,
+                color: context.colorScheme.primary,
+                fillColor: context.colorScheme.primary,
+                borderColor: context.colorScheme.onPrimary,
+                selectedBorderColor: context.colorScheme.primary,
+                constraints: const BoxConstraints(minWidth: 67, minHeight: 24),
+                children: const [Text('عربي'), Text('English')],
+              ),
+            ),
+            SizedBox(height: 16),
+            Text('الدعم', style: TextStyle(fontSize: 16)),
+
+            OptionCard(
+              leadingIcon: HugeIcons.strokeRoundedCustomerService01,
+              text: 'الدعم الفني',
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_back_ios_rounded),
+              ),
+            ),
+            OptionCard(
+              leadingIcon: HugeIcons.strokeRoundedPropertyEdit,
+              text: 'الاحكام والشروط ',
+              trailing: IconButton(
+                onPressed: () {
+                  context.pushNamed(RouteNames.termsConditionsScreen);
+                },
+                icon: Icon(Icons.arrow_back_ios_rounded),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            SizedBox(height: 5),
+            OptionCard(
+              text: 'حذف الحساب',
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_back_ios_rounded),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
