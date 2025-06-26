21e2aa60aa7c7cc839a1b9ce4452bf600b98b1cb
mortada <mortada97442@gmail.com>
2025-06-26 19:08:52 +0300

    fixed the navbar layout

--- lib/view/main_shell.dart
+++ lib/view/main_shell.dart
@@ -28,91 +28,94 @@ class MainShell extends ConsumerWidget {
 
     return Scaffold(
       body: child,
-      bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(15),
-          topRight: Radius.circular(15),
-        ),
-        child: BottomNavigationBar(
-          currentIndex: currentIndex,
-          onTap: (index) {
-            switch (index) {
-              case 0:
-                context.goNamed(RouteNames.homeScreen);
-                break;
-              case 1:
-                context.goNamed(RouteNames.sponsorshipScreen);
-                break;
-              case 2:
-                context.goNamed(RouteNames.donationScreen);
-                break;
-              case 3:
-                context.goNamed(RouteNames.newsScreen);
-                break;
-              case 4:
-                context.goNamed(RouteNames.profileScreen);
-                break;
-            }
-          },
-          selectedFontSize: SizeConfig.font11,
-          showSelectedLabels: true,
-          showUnselectedLabels: false,
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: context.colorScheme.primary,
-          elevation: 15,
-          backgroundColor: Theme.of(context).colorScheme.surface,
-          items: [
-            BottomNavigationBarItem(
-              icon: HugeIcon(
-                icon: HugeIcons.strokeRoundedHome03,
-                color:
-                    currentIndex == 0
-                        ? context.colorScheme.primary
-                        : context.colorScheme.shadow,
+      bottomNavigationBar: Directionality(
+        textDirection: TextDirection.rtl,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
+          ),
+          child: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (index) {
+              switch (index) {
+                case 0:
+                  context.goNamed(RouteNames.homeScreen);
+                  break;
+                case 1:
+                  context.goNamed(RouteNames.sponsorshipScreen);
+                  break;
+                case 2:
+                  context.goNamed(RouteNames.donationScreen);
+                  break;
+                case 3:
+                  context.goNamed(RouteNames.newsScreen);
+                  break;
+                case 4:
+                  context.goNamed(RouteNames.profileScreen);
+                  break;
+              }
+            },
+            selectedFontSize: SizeConfig.font11,
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: context.colorScheme.primary,
+            elevation: 15,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            items: [
+              BottomNavigationBarItem(
+                icon: HugeIcon(
+                  icon: HugeIcons.strokeRoundedHome03,
+                  color:
+                      currentIndex == 0
+                          ? context.colorScheme.primary
+                          : context.colorScheme.shadow,
+                ),
+                label: 'الرئيسة',
               ),
-              label: 'الرئيسة',
-            ),
-            BottomNavigationBarItem(
-              icon: HugeIcon(
-                icon: HugeIcons.strokeRoundedChart01,
-                color:
-                    currentIndex == 1
-                        ? context.colorScheme.primary
-                        : context.colorScheme.shadow,
+              BottomNavigationBarItem(
+                icon: HugeIcon(
+                  icon: HugeIcons.strokeRoundedChart01,
+                  color:
+                      currentIndex == 1
+                          ? context.colorScheme.primary
+                          : context.colorScheme.shadow,
+                ),
+                label: 'سجل الكفالة',
               ),
-              label: 'سجل الكفالة',
-            ),
-            BottomNavigationBarItem(
-              icon: HugeIcon(
-                icon: HugeIcons.strokeRoundedStarAward01,
-                color:
-                    currentIndex == 2
-                        ? context.colorScheme.primary
-                        : context.colorScheme.shadow,
+              BottomNavigationBarItem(
+                icon: HugeIcon(
+                  icon: HugeIcons.strokeRoundedStarAward01,
+                  color:
+                      currentIndex == 2
+                          ? context.colorScheme.primary
+                          : context.colorScheme.shadow,
+                ),
+                label: 'الكفالة',
               ),
-              label: 'الكفالة',
-            ),
-            BottomNavigationBarItem(
-              icon: HugeIcon(
-                icon: HugeIcons.strokeRoundedComputer,
-                color:
-                    currentIndex == 3
-                        ? context.colorScheme.primary
-                        : context.colorScheme.shadow,
+              BottomNavigationBarItem(
+                icon: HugeIcon(
+                  icon: HugeIcons.strokeRoundedComputer,
+                  color:
+                      currentIndex == 3
+                          ? context.colorScheme.primary
+                          : context.colorScheme.shadow,
+                ),
+                label: 'الاخبار',
               ),
-              label: 'الاخبار',
-            ),
-            BottomNavigationBarItem(
-              icon: HugeIcon(
-                icon: HugeIcons.strokeRoundedUser,
-                color:
-                    currentIndex == 4
-                        ? context.colorScheme.primary
-                        : context.colorScheme.shadow,
+              BottomNavigationBarItem(
+                icon: HugeIcon(
+                  icon: HugeIcons.strokeRoundedUser,
+                  color:
+                      currentIndex == 4
+                          ? context.colorScheme.primary
+                          : context.colorScheme.shadow,
+                ),
+                label: 'حسابي',
               ),
-              label: 'حسابي',
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
