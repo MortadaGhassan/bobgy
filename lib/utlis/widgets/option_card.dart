e25a8a9071cc9c7d071b9bae42dff5e524e812ff
mortada <mortada97442@gmail.com>
2025-06-29 17:22:46 +0300

    added onTap so the whole card is tappable

--- lib/utlis/widgets/option_card.dart
+++ lib/utlis/widgets/option_card.dart
@@ -7,39 +7,47 @@ class OptionCard extends StatelessWidget {
   final IconData? leadingIcon;
   final String text;
   final Widget trailing;
+  final onTap;
 
   const OptionCard({
     super.key,
     this.leadingIcon,
     required this.text,
     required this.trailing,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 9.h,
-      width: double.infinity,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-          side: BorderSide(color: context.colorScheme.onSecondary, width: 1.5),
-        ),
-        elevation: 5,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
-          child: Row(
-            children: [
-              if (leadingIcon != null) ...[
-                HugeIcon(
-                  icon: leadingIcon!,
-                  color: context.colorScheme.primary,
-                ),
-                const SizedBox(width: 15),
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        height: 9.h,
+        width: double.infinity,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: BorderSide(
+              color: context.colorScheme.onSecondary,
+              width: 1.5,
+            ),
+          ),
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+            child: Row(
+              children: [
+                if (leadingIcon != null) ...[
+                  HugeIcon(
+                    icon: leadingIcon!,
+                    color: context.colorScheme.primary,
+                  ),
+                  const SizedBox(width: 15),
+                ],
+                Expanded(child: Text(text, textAlign: TextAlign.start)),
+                trailing,
               ],
-              Expanded(child: Text(text, textAlign: TextAlign.start)),
-              trailing,
-            ],
+            ),
           ),
         ),
       ),
